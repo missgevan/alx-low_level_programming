916916af7be7f977d566ee82c12acf4db1887393
missgevan <leviagevansekyi@gmail.com>
2022-09-11 11:28:14 -0700

    Printing hexadecimal numbers using putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char hexa;
+for (hexa = '0' ; hexa < '10' ; hexa++)
+{
+	putchar(hexa);
+}
+for (hexa = 'a' ; hexa <= 'f' ; hexa++)
+{
+	putchar(hexa);
+}
+putchar('\n');
+return (0);
+}
