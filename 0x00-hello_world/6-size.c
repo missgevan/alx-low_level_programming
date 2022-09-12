3ab1e1af1584a204eb7abe2c878b91aca382b932
missgevan <leviagevansekyi@gmail.com>
2022-09-12 11:01:41 -0700

    Prints size of different types on computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,7 @@ float y;
 printf("Size of a char: %d byte(s)\n", sizeof(u));
 printf("Size of an int: %d byte(s)\n", sizeof(v));
 printf("Size of a long int: %d bytes(s)\n", sizeof(w));
-printf("Size of a long long int: %d byte(s)\n", sizeof(x));printf("Size of a float: %d bytes(s)\n", sizeof(y));
+printf("Size of a long long int: %d byte(s)\n", sizeof(x));
+printf("Size of a float: %d bytes(s)\n", sizeof(y));
 return (0);
 }
