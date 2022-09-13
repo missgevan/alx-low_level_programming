cef9dfcdc481bec4d77329074f68120549a0aaac
missgevan <leviagevansekyi@gmail.com>
2022-09-13 15:40:55 -0700

    Printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Printing _putchar using the putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
