85687844d417cae9b2b0873d70cd38f30817f09b
missgevan <leviagevansekyi@gmail.com>
2022-09-13 16:15:50 -0700

    Printing lowercase alphabets using _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Sucess)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char c;
 for (c = 'a' ; c >= 'z' ; c++)
@@ -12,4 +12,5 @@ for (c = 'a' ; c >= 'z' ; c++)
 _putchar(c);
 }
 _putchar('\n');
+return (0);
 }
