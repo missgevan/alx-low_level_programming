5b49878867314ebac48c718e0d41bee77909c014
missgevan <leviagevansekyi@gmail.com>
2022-09-14 13:58:46 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,9 @@
 void times_table(void)
 {
 int a, b, c;
+
 for (a = 0; a <= 9; a++)
+{
 _putchar('0');
 
 for (b = 1; b <= 9; b++)
@@ -19,10 +21,11 @@ c = a * b;
 
 if (c <= 9)
 _putchar(' ');
-
 else
 _putchar((c / 10) + '0');
+
 _putchar((c % 10) + '0');
 }
 _putchar('\n');
+}
 }
