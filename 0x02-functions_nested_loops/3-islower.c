0e9c543120592436462b183a3bb50ca03d5022f7
missgevan <leviagevansekyi@gmail.com>
2022-09-13 19:17:47 -0700

    Checks for a character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _islower(int c) - Checks for a character.
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+{
+else
+}
+return (0);
+}
