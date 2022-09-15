572cea2beda2bc7802623d410018916da7d201c3
missgevan <leviagevansekyi@gmail.com>
2022-09-14 20:52:49 -0700

    Printing every minute in 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minutes starting from 00:00 to 23:59
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int m = 0;
+	int h = 0;
+	int M;
+	int H;
+while (H <= 23)
+{
+while (M <= 59)
+{
+	H = h % 10;
+	_putchar(h / 10 + '0');
+	_putchar(H + '0');
+	_putchar(':');
+	_putchar(m / 10 + '0');
+	_putchar(M + '0');
+	m++;
+	_putchar('\n');
+}
+h++;
+m = 0;
+}
+}
