2fdd46dff15914ca240bdf90aa421c5705666b34
missgevan <leviagevansekyi@gmail.com>
2022-09-16 16:16:54 -0700

    Printing numbers 0 to 9 and a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints 0 to 9 and a new line
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
