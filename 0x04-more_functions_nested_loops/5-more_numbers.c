5ca0f574c325462c23f6fed9695941817a01ae74
missgevan <leviagevansekyi@gmail.com>
2022-09-17 08:26:04 -0700

    Printing 0 to 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,8 @@ void more_numbers(void)
 		for (b = '0' ; b <= 14 ; b++)
 		{
 			if (b > 9)
-				b = a / 10;
 			{
-				_putchar(b + '0');
+				_putchar((b / 10) + '0');
 			}
 		_putchar((b % 10) + '0');
 		}
