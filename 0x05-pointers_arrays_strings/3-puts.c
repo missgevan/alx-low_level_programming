d2fd3090b50979f6b8b90e1803f32df14994e51f
missgevan <leviagevansekyi@gmail.com>
2022-09-19 19:59:36 -0700

    Printing a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,9 +11,9 @@ void _puts(char *str)
 {
 	int string_length = 0;
 
-		while (*(str + string_lenth) != '\0')
+		while (*(str + string_length) != '\0')
 		{
-			_putchar(*(str + string_lenth));
+			_putchar(*(str + string_length));
 			string_length++;
 		}
 		_putchar('\n');
