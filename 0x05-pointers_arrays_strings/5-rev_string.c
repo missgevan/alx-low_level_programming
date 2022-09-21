3cf827fdc6b04f5609ec5700620d32dd34e340e0
missgevan <leviagevansekyi@gmail.com>
2022-09-21 08:31:20 -0700

    String reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,26 +9,20 @@
 
 void rev_string(char *s)
 {
-	int i, j, k;
-	char *a, b;
+	int a = 0, i, j;
+	char c;
 
-	a = s;
-
-	while (s[j] != '\0')
-	{
-		j++;
-	}
-
-	for (k = 1 ; k < j ; k++)
+	while (*(s + a) != '\0')
 	{
 		a++;
 	}
-
-	for (i = 0 ; i < j / 2 ; i++)
+	j = a;
+	a--;
+	for (i = 0 ; i < (j / 2) ; i++)
 	{
-		b = s[i];
-		s[i] = *a;
-		*a = b;
+		c = *(s + i);
+		*(s + i) = *(s + a);
+		*(s + a) = c;
 		a--;
 	}
 }
