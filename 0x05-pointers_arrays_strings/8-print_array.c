d2d0767507210a7f8d4e50d7ff75aa52ccc75939
missgevan <leviagevansekyi@gmail.com>
2022-09-25 14:12:00 -0700

    Printing a number of elemets of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,14 +9,16 @@
 
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0 ; i < n ; i++)
 	{
-		if (i < n - 1)
+		while (i < n)
 			printf("%d", a[i]);
-		else
-			printf("%d, ", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+		i++;
 	}
 	printf("\n");
 }
