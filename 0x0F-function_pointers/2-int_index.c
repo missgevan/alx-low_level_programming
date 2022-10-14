8717683a22fbbc4c02f41b1296cc99d863166193
missgevan <leviagevansekyi@gmail.com>
2022-10-14 14:33:29 -0700

    Code edit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,12 +11,13 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
 	if (array && cmp)
 	{
 		if (size <= 0)
 			return (-1);
+		i = 0;
 		while (i < size)
 			i++;
 		if (cmp(array[i]))
