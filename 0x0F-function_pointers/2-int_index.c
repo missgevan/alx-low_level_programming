7fd142ecf4041eadf044c8b599b9f55f8e88374b
missgevan <leviagevansekyi@gmail.com>
2022-10-14 16:03:07 -0700

    Perform simple operations

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,11 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		if (size <= 0)
 			return (-1);
-		i = 0;
-		{
-		while (i < size)
-			i++;
-		}
+		for (i = 0; i < size; i++)
 		if (cmp(array[i]))
 			return (i);
 	}
