7891530069bbc066e188694493fb91cd9a1b92b3
missgevan <leviagevansekyi@gmail.com>
2022-10-28 16:39:38 -0700

    Code edit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int sum, expo;
 	int i;
 
-	for (i = 0; b[i]; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
