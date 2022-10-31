cbcf08a420ae8f3ae943ca4769ab218e8a0307c9
missgevan <leviagevansekyi@gmail.com>
2022-10-31 11:20:35 -0700

    Edits

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,16 @@
 
 void more_numbers(void)
 {
-	int a, b;
+	int i, j;
 
-	for (a = 0 ; a < 10 ; a++)
+	for (i = 0 ; i <= 10 ; i++)
 	{
-		for (b = '0' ; b <= 14 ; b++)
+		for (j = '0' ; j <= 14 ; j++)
 		{
-			if (b > 9)
-			{
-				_putchar((b / 10) + '0');
-			}
-		_putchar((b % 10) + '0');
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,29 +2,10 @@
 
 /**
  * main - Entry point
- * prints numbers, fizz, buzz or fizzbuzz
+ * Prints FizzBuzz, Fizz, Buzz
  *
  * Return: Void
  */
 
 int main(void)
-{
-	int n;
-
-	for (n = 1 ; n <= 100 ; n++)
-		while (n < 101)
-
-		{
-			if (n % 3 == 0 && n % 5 == 0)
-				printf("FizzBuzz");
-			else if (n % 3 == 0)
-				printf("Fizz");
-			else if (n % 5 == 0)
-				printf("Buzz");
-			else
-				printf(" %d", n);
-			n++;
-		}
-	printf("\n");
-	return (0);
-}
+	:wq
